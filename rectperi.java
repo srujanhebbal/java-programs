5b4d1360e58293c4be2ec29d3190b8345dd54a9a
srujanhebbal <srujanhebbal@gmail.com>
2026-09-07 22:03:17 +0530

    Add Java basic programs

---
+++ rectperi.java
@@ -0,0 +1,9 @@
+public class rectperi {
+    public static void main(String[] args) {
+        int length = 5;
+        int breadth = 10;
+        int perimeter = 2 * (length + breadth);
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+    }
+    
+}
